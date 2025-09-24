1c01eb7c3516e4648a1524068eb023b89b37f2f1
slim <ouertani@gmail.com>
2025-09-24 11:14:45 +0100

    chore: adding f types

--- rclap_core/src/utils.rs
+++ rclap_core/src/utils.rs
@@ -1,6 +1,6 @@
-const NATIVE_TYPES: [&str; 16] = [
+const NATIVE_TYPES: [&str; 19] = [
     "i8", "i16", "i32", "i64", "i128", "isize", "u8", "u16", "u32", "u64", "u128", "usize", "bool",
-    "String", "OsString", "PathBuf",
+    "f32", "f64", "f128", "String", "OsString", "PathBuf",
 ];
 pub(crate) fn is_native_type(ty: &str) -> bool {
     NATIVE_TYPES.contains(&ty)
