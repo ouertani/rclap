fa1eeb3f76e5657de283a7dea5d9f265a4ef3268
slim <ouertani@gmail.com>
2025-09-25 15:41:01 +0100

    full path for pathbuf

--- rclap_core/src/utils.rs
+++ rclap_core/src/utils.rs
@@ -1,6 +1,23 @@
 const NATIVE_TYPES: [&str; 19] = [
-    "i8", "i16", "i32", "i64", "i128", "isize", "u8", "u16", "u32", "u64", "u128", "usize", "bool",
-    "f32", "f64", "f128", "String", "OsString", "PathBuf",
+    "i8",
+    "i16",
+    "i32",
+    "i64",
+    "i128",
+    "isize",
+    "u8",
+    "u16",
+    "u32",
+    "u64",
+    "u128",
+    "usize",
+    "bool",
+    "f32",
+    "f64",
+    "f128",
+    "String",
+    "OsString",
+    "std::path::PathBuf",
 ];
 pub(crate) fn is_native_type(ty: &str) -> bool {
     NATIVE_TYPES.contains(&ty)
